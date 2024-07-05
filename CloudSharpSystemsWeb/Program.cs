21ed88f3ba2d54a0113015f2a73edaa2ea42e31e
Shu-Yuan Yang <zantgoron@gmail.com>
2024-07-05 16:42:16 +0800

    Removed SSO login redirect middleware from CloudSharpSystemsWeb micro services.

--- CloudSharpSystemsWeb/Program.cs
+++ CloudSharpSystemsWeb/Program.cs
@@ -1,31 +1,20 @@
 using APIConnector.Model;
 using AuxiliaryClassLibrary.ExceptionHelper;
-using CloudSharpSystemsWeb.CustomMiddleWares;
 using DBConnectionLibrary;
 using DBConnectionLibrary.DBQueryContexts;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using System.Net;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add json for query validator:
 builder.Configuration.AddJsonFile("dynamic-query-config.json", optional: false, reloadOnChange: false);
 builder.Configuration.AddJsonFile("gcp_service_account_secrets.json", optional: true, reloadOnChange: false);
-builder.Configuration
+/*builder.Configuration
     .AddJsonFile("gcp_credentials_client_secrets.json", optional: true, reloadOnChange: false)
     .AddJsonFile($"gcp_credentials_client_secrets.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false);
+*/
 
 
-// Add services to the container.
-// Shu-Yuan Yang 20240513 added log in options
-//builder.Services.AddAuthentication().AddGoogle(googleOptions =>
-//{
-//    googleOptions.ClientId = "466344244859-uidehqsrjah6ae0dse457vrdqbc0eovf.apps.googleusercontent.com"; // configuration["Authentication:Google:ClientId"];
-//    googleOptions.ClientSecret = "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"; //configuration["Authentication:Google:ClientSecret"];
-//});
 
 
 
@@ -61,7 +50,7 @@ builder.Services.AddTransient<ExceptionMiddleware>();
 
 // Link Entity Framework Core DB Context to establish reference to the App database and storage:
 builder.Services.Configure<GCPServiceAccountSecretKeyObject>(builder.Configuration); // get storage service account credential config
-builder.Services.Configure<GCPOAuth2ClientSecretKeyObject>(builder.Configuration); // get Google credentials client secret config
+//builder.Services.Configure<GCPOAuth2ClientSecretKeyObject>(builder.Configuration); // get Google credentials client secret config
 builder.Services.Configure<DynamicQueryConfig>(builder.Configuration); // get queryable fields config
 builder.Services.AddDbContext<AppDBMainContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseAppMainConnectionString")));
 
@@ -77,14 +66,14 @@ builder.Services.AddCors();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 
-// 20240513 Enable Session Management:
-builder.Services.AddDistributedMemoryCache();
+// 20240705 Disable Session Management:
+/*builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromSeconds(60);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
-});
+});*/
 
 
 
@@ -122,11 +111,6 @@ app.UseCors(builder =>
 });
 
 app.UseAuthorization();
-
-app.UseSession();
-
-// Shu-Yuan Yang 20240513 redirect middleware:
-app.UseMiddleware<RedirectMiddleWare>();
 
 // use global exception handling middleware:
 app.UseMiddleware<ExceptionMiddleware>();
