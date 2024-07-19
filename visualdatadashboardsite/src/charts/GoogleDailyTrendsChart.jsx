1d51e6e5047007fcfb11ad579520f56c58bb2b1d
Shu-Yuan Yang <zantgoron@gmail.com>
2024-07-19 20:53:22 +0800

    Added source info as title in Google Trends chart.

--- visualdatadashboardsite/src/charts/GoogleDailyTrendsChart.jsx
+++ visualdatadashboardsite/src/charts/GoogleDailyTrendsChart.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect, useMemo, useCallback } from "react";
-//import { time_to_string_HHmmss } from "../auxiliary/time/timehelper";
-//import LineChartStandard from "./LineChartStandard";
+import { Link } from "react-router-dom";
 import useFetch from "../endpoints/api/useFetch";
 import { api_full_path, get_api } from "../endpoints/api/api_helper";
 import { APIEndpoints } from "../site_config.json";
 import TableList from "../tables/TableList";
 
 
-const GoogleDailyTrendsChart = ({ title, refreshTriggered, updateRefreshStatus = null }) => {
+const GoogleDailyTrendsChart = ({ title, refreshTriggered, updateRefreshStatus }) => {
 
     const { data: trendsData, refreshData: refreshTrendsData, isPending: isTrendsDataPending, error: trendsDataFetchError } = useFetch(api_full_path(APIEndpoints.CloudSharpMicroService.url, get_api(APIEndpoints.CloudSharpMicroService, "get_google_daily_trends").path));
+
+    useEffect(() => {
+        refreshTrendsData();
+    }, [refreshTriggered, refreshTrendsData]);
 
     const format_trend = useCallback((search) => {
         return {
@@ -36,9 +39,19 @@ const GoogleDailyTrendsChart = ({ title, refreshTriggered, updateRefreshStatus =
     return (
         <div className="container full-height">
             <div className="container full-height scroll-control-y">
-                <div className="container align-center">
-                    <TableList contentList={formattedTrends} refreshDisplay={refreshTrendsData} />
+                <div className="menu-header large">
+                    <span>Data Source: </span>
+                    <Link target="_blank" to={"https://trends.google.com/trends/trendingsearches/daily"}>https://trends.google.com/trends/trendingsearches/daily</Link>
+                    <span>  </span>
+                    {!isTrendsDataPending && trendsData &&
+                        <span>at ({trendsData.FEED_TIME})</span>
+                    }
                 </div>
+                {!isTrendsDataPending && 
+                    <div className="container align-center">
+                        <TableList contentList={formattedTrends} refreshDisplay={refreshTrendsData} />
+                    </div>
+                }
                 <div style={{ whiteSpace: "pre-line" }}>{
                     isTrendsDataPending ? "Loading..." : ""
                 }</div>
